111ecb14a3a48eab51fbb30358471dc07e36fb44
Minh Hai Le <leminhhai220709@gmail.com>
2025-02-17 18:08:10 +0200

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,12 +3,17 @@
 
 import './style.css'
 const MyComponent = () => {
+    const hoidanit = [1, 2, 3]
+
     return (
         <>
-            <div className="">lehai & lehai update 12</div> {/*hello */}
+            <div> {JSON.stringify(hoidanit)} & lehai & lehai update 12</div> {/*hello */}
+            <div>{console.log("Eric")}</div>
             <div className="child"
                 style={
-                    { borderRadius: "10px" }
+                    {
+                        borderRadius: "10px",
+                    }
                 }
             >child</div>
         </>
