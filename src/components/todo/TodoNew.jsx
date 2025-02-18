b0658d88d3b9a0ccd5a69fcd24718fc66e3d3d7c
Minh Hai Le <leminhhai220709@gmail.com>
2025-02-18 14:31:19 +0200

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,6 +3,15 @@ const TodoNew = (props) => {
     const { addNewTodo } = props;
 
     // addNewTodo("ERIC");
+
+    const handleClick = () => {
+        alert("click me")
+    }
+
+    const handleChange = (name) => {
+        console.log(name)
+    }
+
     return (
         <div className='todo-new'>
             <input style={
@@ -10,8 +19,14 @@ const TodoNew = (props) => {
                     height: '27px',
                     width: '22%'
                 }
-            } type="text" />
-            <button className='button'>Add</button>
+            } type="text"
+                onChange={(e) => {
+                    handleChange(e.target.value)
+                }}
+            />
+            <button className='button'
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
