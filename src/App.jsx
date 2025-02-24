fd2bd166ef36a80ec0507e5418b4df76754e5825
Minh Hai Le <leminhhai220709@gmail.com>
2025-02-24 15:13:58 +0200

    video 52

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import './components/todo/todo.css'
-import TodoData from './components/todo/TodoData'
-import reactLogo from './assets/react.svg'
-import TodoNew from './components/todo/TodoNew'
-import { useState } from 'react'
 import Header from './components/layout/header'
 import Footer from './components/layout/footer'
 import { Outlet } from 'react-router-dom'
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import LoginPage from './pages/login.jsx'
 import RegisterPage from './pages/register.jsx'
 import UserPage from './pages/user.jsx'
-import ProductPage from './pages/product.jsx'
+import BookPage from './pages/book.jsx'
 import './styles/global.css'
 import TodoApp from './components/todo/TodoApp.jsx'
+import ErrorPage from './pages/error.jsx'
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -27,8 +29,8 @@ const router = createBrowserRouter([
       },
 
       {
-        path: "/products",
-        element: <ProductPage />
+        path: "/books",
+        element: <BookPage />
       },
     ]
   },
@@ -50,7 +52,7 @@ const router = createBrowserRouter([
 
   // {
   //   path: "/products",
-  //   element: <ProductPage />
+  //   element: <BookPage />
   // },
 
 ])
