1f67db5a59498bde0458eaafe54e17c8c23cea26
Minh Hai Le <leminhhai220709@gmail.com>
2025-02-24 14:25:56 +0200

    video 50

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,13 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './header.css'
 
 const Header = () => {
     return (
         <ul>
-            <li><Link className="active" to="/">Home</Link></li>
-            <li><Link to="/users">Users</Link></li>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/users">Users</NavLink></li>
+            <li><NavLink to="/products">Products</NavLink></li>
 
-            <li><Link to="/products">Products</Link></li>
         </ul>
 
     )
