268298089300e323c62fd8f1bef4004ff9fbfd0b
Minh Hai Le <leminhhai220709@gmail.com>
2025-03-02 18:16:44 +0200

    video 73

--- src/main.jsx
+++ src/main.jsx
@@ -64,10 +64,9 @@ const router = createBrowserRouter([
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <RouterProvider router={router} /> */}
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <RouterProvider router={router} />
+  // </React.StrictMode>,
 )
 
 
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -18,7 +18,16 @@ const updateUserApi = () => {
 
 }
 
+const fetchAllUserAPI = () => {
+    const URL_BACKEND = '/api/v1/user';
+
+
+    const res = axios.get(URL_BACKEND)
+    return res;
+}
+
 export {
     createUserAPI,
     updateUserApi,
+    fetchAllUserAPI
 }
